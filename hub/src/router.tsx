bf23ba654d648f1eeeababf5bfebb30401b03528
oparap8 <oparap8@gmail.com>
2026-09-18 13:58:00 +0000

    feat: add dashboard stub to router

--- hub/src/router.tsx
+++ hub/src/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 
 import HubLayout from './layouts/HubLayout'
+import Dashboard from './pages/dashboard'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HubLayout />}>
-      <Route index element={<h1>Home</h1>} />
+      <Route index element={<Dashboard />} />
       <Route path="/about" element={<h1>About</h1>} />
     </Route>
   )
